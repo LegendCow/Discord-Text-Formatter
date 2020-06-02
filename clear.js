572c05ad5b6fd954e11183b8b4beb29a1a7849fd
LegendCow <productivestuffandcows@gmail.com>
2020-06-02 00:03:37 -0400

    everything works now!

---
+++ clear.js
@@ -0,0 +1,7 @@
+function clearSelectors() {
+  document.getElementById("italic").checked = false;
+  document.getElementById("bold").checked = false;
+  document.getElementById("underline").checked = false;
+  document.getElementById("strikethrough").checked = false;
+  document.getElementById("spoiler").checked = false;
+}
--- myScript.js
+++ myScript.js
@@ -30,13 +30,105 @@ $(document).ready(function(){
     var underline = document.getElementById("underline").checked;
     var strikethrough = document.getElementById("strikethrough").checked;
     var spoiler = document.getElementById("spoiler").checked;
-    if (italic == true) {
-      document.getElementById("preview").style.fontStyle = "italic";
-      document.getElementById("output").innerHTML = "*" + $('#input').val() + "*";
+    var input = $('#input').val();
+    if (spoiler == true) {
+      var input = "||" + $('#input').val() + "||";
     }
-    else {
+    if (italic == false && bold == false && underline == false && strikethrough == false) {
       document.getElementById("preview").style.fontStyle = "normal";
-      document.getElementById("output").innerHTML = $('#input').val();
+      document.getElementById("preview").style.fontWeight = "normal";
+      document.getElementById("preview").style.textDecoration = "none";
+      document.getElementById("output").innerHTML = input;
+    }
+    if (italic == true && bold == false && underline == false && strikethrough == false) {
+      document.getElementById("preview").style.fontStyle = "italic";
+      document.getElementById("preview").style.fontWeight = "normal";
+      document.getElementById("preview").style.textDecoration = "none";
+      document.getElementById("output").innerHTML = "*" + input + "*";
+    }
+    if (italic == false && bold == true && underline == false && strikethrough == false) {
+      document.getElementById("preview").style.fontStyle = "normal";
+      document.getElementById("preview").style.fontWeight = "bold";
+      document.getElementById("preview").style.textDecoration = "none";
+      document.getElementById("output").innerHTML = "**" + input + "**";
+    }
+    if (italic == false && bold == false && underline == true && strikethrough == false) {
+      document.getElementById("preview").style.fontStyle = "normal";
+      document.getElementById("preview").style.fontWeight = "normal";
+      document.getElementById("preview").style.textDecoration = "underline";
+      document.getElementById("output").innerHTML = "__" + input + "__";
+    }
+    if (italic == false && bold == false && underline == false && strikethrough == true) {
+      document.getElementById("preview").style.fontStyle = "normal";
+      document.getElementById("preview").style.fontWeight = "normal";
+      document.getElementById("preview").style.textDecoration = "line-through";
+      document.getElementById("output").innerHTML = "~~" + input + "~~";
+    }
+    if (italic == true && bold == true && underline == false && strikethrough == false) {
+      document.getElementById("preview").style.fontStyle = "italic";
+      document.getElementById("preview").style.fontWeight = "bold";
+      document.getElementById("preview").style.textDecoration = "none";
+      document.getElementById("output").innerHTML = "***" + input + "***";
+    }
+    if (italic == true && bold == false && underline == true && strikethrough == false) {
+      document.getElementById("preview").style.fontStyle = "italic";
+      document.getElementById("preview").style.fontWeight = "normal";
+      document.getElementById("preview").style.textDecoration = "underline";
+      document.getElementById("output").innerHTML = "__*" + input + "*__";
+    }
+    if (italic == true && bold == false && underline == false && strikethrough == true) {
+      document.getElementById("preview").style.fontStyle = "italic";
+      document.getElementById("preview").style.fontWeight = "normal";
+      document.getElementById("preview").style.textDecoration = "line-through";
+      document.getElementById("output").innerHTML = "*~~" + input + "~~*";
+    }
+    if (italic == false && bold == true && underline == true && strikethrough == false) {
+      document.getElementById("preview").style.fontStyle = "normal";
+      document.getElementById("preview").style.fontWeight = "bold";
+      document.getElementById("preview").style.textDecoration = "underline";
+      document.getElementById("output").innerHTML = "__**" + input + "**__";
+    }
+    if (italic == false && bold == true && underline == false && strikethrough == true) {
+      document.getElementById("preview").style.fontStyle = "normal";
+      document.getElementById("preview").style.fontWeight = "bold";
+      document.getElementById("preview").style.textDecoration = "line-through";
+      document.getElementById("output").innerHTML = "**~~" + input + "~~**";
+    }
+    if (italic == false && bold == false && underline == true && strikethrough == true) {
+      document.getElementById("preview").style.fontStyle = "normal";
+      document.getElementById("preview").style.fontWeight = "normal";
+      document.getElementById("preview").style.textDecoration = "underline line-through";
+      document.getElementById("output").innerHTML = "__~~" + input + "~~__";
+    }
+    if (italic == true && bold == true && underline == true && strikethrough == false) {
+      document.getElementById("preview").style.fontStyle = "italic";
+      document.getElementById("preview").style.fontWeight = "bold";
+      document.getElementById("preview").style.textDecoration = "underline";
+      document.getElementById("output").innerHTML = "__***" + input + "***__";
+    }
+    if (italic == true && bold == false && underline == true && strikethrough == true) {
+      document.getElementById("preview").style.fontStyle = "italic";
+      document.getElementById("preview").style.fontWeight = "normal";
+      document.getElementById("preview").style.textDecoration = "underline line-through";
+      document.getElementById("output").innerHTML = "__*~~" + input + "~~*__";
+    }
+    if (italic == true && bold == true && underline == false && strikethrough == true) {
+      document.getElementById("preview").style.fontStyle = "italic";
+      document.getElementById("preview").style.fontWeight = "bold";
+      document.getElementById("preview").style.textDecoration = "line-through";
+      document.getElementById("output").innerHTML = "***~~" + input + "~~***";
+    }
+    if (italic == false && bold == true && underline == true && strikethrough == true) {
+      document.getElementById("preview").style.fontStyle = "normal";
+      document.getElementById("preview").style.fontWeight = "bold";
+      document.getElementById("preview").style.textDecoration = "underline line-through";
+      document.getElementById("output").innerHTML = "__**~~" + input + "~~**__";
+    }
+    if (italic == true && bold == true && underline == true && strikethrough == true) {
+      document.getElementById("preview").style.fontStyle = "italic";
+      document.getElementById("preview").style.fontWeight = "bold";
+      document.getElementById("preview").style.textDecoration = "underline line-through";
+      document.getElementById("output").innerHTML = "__***~~" + input + "~~***__";
     }
   }
 });
